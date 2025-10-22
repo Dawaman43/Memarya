c57cbf3ee75329d2473a4e7ef264cf5c6399b6d2
Dawaman43 <dawitworkujima@gmail.com>
2025-10-22 08:08:28 +0000

    Oct 22, 2025, 11:08 AM

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/client";
 
 export const authClient = createAuthClient({
-  baseURL: "/api/auth",
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL + "/api/auth",
 });
 
 export const { signIn, signOut, signUp, useSession } = authClient;
