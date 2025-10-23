3b3e7cbb46390b6352667ea3e27c30cf44082ef4
Dawaman43 <dawitworkujima@gmail.com>
2025-10-23 07:29:21 +0000

    Add user information display to the profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,11 +3,24 @@ import { useSession } from "@/lib/auth-client";
 
 function ProfilePage() {
   const { data: session } = useSession();
-
+  const user = session?.user;
   return (
     <div>
       <h1>Profile Page</h1>
       <p>Welcome to your profile!</p>
+      {user && (
+        <div>
+          <h2>Your Information</h2>
+          <p>Name: {user.name}</p>
+          <p>Email: {user.email}</p>
+          <img
+            src={user.image ?? ""}
+            alt="User Image"
+            width={100}
+            height={100}
+          />
+        </div>
+      )}
     </div>
   );
 }
